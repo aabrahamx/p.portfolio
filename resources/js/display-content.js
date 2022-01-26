d978c5311c5365c32c040ec9d0c84070220619a9
Abel A <abelabrahamx@gmail.com>
2022-01-26 05:06:37 -0600

    cleaned document

--- resources/js/display-content.js
+++ resources/js/display-content.js
@@ -1,25 +1,5 @@
 
-//this js document sole purpose is to display hidden faq styled content
-
-/*
-
-const arrow = document.querySelector('.arrow-1');
-const content = document.querySelector('.sec-low-1');
-
-
-arrow.style.cursor = 'pointer';
-
-
-arrow.addEventListener("click", () => {
-    if (content.style.display == 'block') {
-        content.style.display = 'none';
-    } else if (content.style.display == 'none') {
-        content.style.display = 'block';
-    }
-});
-
-*/
-
+//this document is to display the faq styled dropdowns. ex: about me page.
 
 let answers = document.querySelectorAll("section");
 answers.forEach((event) => {

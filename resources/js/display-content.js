06c09e135c5eaf7f32ab8f2fa097fac1d164280e
Abel A <abelabrahamx@gmail.com>
2022-02-11 14:34:23 -0600

    extended functionality

--- resources/js/display-content.js
+++ resources/js/display-content.js
@@ -1,14 +1,23 @@
 
-//this document is to display the faq styled dropdowns. ex: about me page.
 
-let answers = document.querySelectorAll("section");
-answers.forEach((event) => {
-    event.addEventListener("click", () => {
-        if (event.classList.contains("active")) {
-            event.classList.remove("active");
+$(document).ready(()=> {
+
+    const $sections = $('section');
+    $sections.click((event)=> {
+        $(event.currentTarget).toggleClass('active');
+        $(event.currentTarget).siblings().removeClass('active');
+        if($(event.currentTarget).hasClass('active')) {
+            // dependent animation on css file
+            // animation on css: 2.5s 
+            $('.top-content').hide();
+            delayId = window.setTimeout(() => {
+                $(event.currentTarget).find('.top-content').slideDown(1000);
+            }, 1500)
+
         } else {
-            event.classList.add("active");
+            window.clearTimeout(delayId);
+            $('.top-content').hide();
         }
-    });
+    })
 });
 

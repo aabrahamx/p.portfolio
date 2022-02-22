a4245ddb61c12b38f0b63c20ec5d60f0559f09fe
Abel A <abelabrahamx@gmail.com>
2022-02-22 11:54:54 -0600

    minor debugging

--- resources/js/aboutme.js
+++ resources/js/aboutme.js
@@ -1,10 +1,7 @@
 
 $(document).ready(() => {
-
     $('section').click((event) => {
-        console.log('clicked')
         $(event.currentTarget).toggleClass('active');
         $(event.currentTarget).siblings().removeClass('active')
     })
-
 })

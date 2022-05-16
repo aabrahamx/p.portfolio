cc9bb3e6af1b9013f0c0e61f501bb1fcc5446b37
Abel A <abelabrahamx@gmail.com>
2022-05-16 01:03:55 -0500

    rebuild

---
+++ src/components/Hero/Hero.vue
@@ -0,0 +1,45 @@
+<script setup>
+  import IconLink from '@/components/IconLink/IconLink.vue';
+</script>
+
+<template>
+  <section
+    class="flex flex-col items-center justify-center w-full h-screen mb-20"
+  >
+    <div class="content">
+      <h1 class="font-black text-7xl">
+        Hi, I'm <span class="text-light-yellow">Abel</span>.
+      </h1>
+      <h2 class="mb-10 text-4xl font-light tracking-wide opacity-75 mt-7">
+        Front-End Developer
+      </h2>
+      <IconLink
+        title="GitHub"
+        href="https://github.com/abelabrahamx"
+        src="https://img.icons8.com/ios-glyphs/90/ffffff/github.png"
+      />
+      <IconLink
+        title="CodePen"
+        href="https://codepen.io/abelg939"
+        src="https://img.icons8.com/ios-filled/100/ffffff/codepen.png"
+      />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  .content {
+    animation: slide-in 1s linear;
+  }
+
+  @keyframes slide-in {
+    from {
+      top: 10%;
+      opacity: 0;
+    }
+    to {
+      top: 0;
+      opacity: 1;
+    }
+  }
+</style>
---
+++ src/components/IconLink/IconLink.vue
@@ -0,0 +1,56 @@
+<script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+  });
+</script>
+
+<template>
+  <a
+    class="text-sm font-bold border icon-link bg-white/5 border-white/40"
+    :href="props.href"
+  >
+    <img class="icon" :alt="props.title + 'icon'" :src="props.src" />
+    <span class="title">{{ props.title }}</span>
+  </a>
+</template>
+
+<style scoped>
+  .icon-link {
+    display: inline-flex;
+    margin-right: 2rem;
+    width: 10rem;
+    height: 3rem;
+    align-items: center;
+    justify-content: center;
+    border-radius: 0.25rem;
+    transition: all 0.3s linear;
+    cursor: pointer;
+  }
+
+  .icon-link:hover {
+    opacity: 1;
+    background: none;
+  }
+
+  .icon,
+  .title {
+    margin: 0.2rem;
+  }
+
+  .icon {
+    width: 1.25rem;
+  }
+</style>
